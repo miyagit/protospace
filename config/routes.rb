1ae170f81015b85c9d2d474537874c35e1792df5
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-07-03 16:11:22 +0900

    controller define action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
 	resources :prototypes, only: [:index, :show, :new]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
