9fd0e9b3d64802b10e45de725adf819cfb1f7196
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-07-06 10:51:56 +0900

    fix indent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:avatar, :job, :profile, :title, :name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :job, :title, :profile, :name])
-  end
+ end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def edit
+  def edit
     @user = current_user
-	end
+  end
 
   def update
     @user = current_user
