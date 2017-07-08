394319a2131b2e6421d200fcb76c4791ff62c108
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-07-08 17:26:16 +0900

    link to show from index implement

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 	devise_for :users
 	root 'prototypes#index'
-	resources :prototypes, only: %w(show new create)
-	resources :users, only: %w(show edit update)
+
+	resources :prototypes, only: %w(index new edit show create update)
+	resources :users, only: %w(show edit update, delete)
+
 end
 
